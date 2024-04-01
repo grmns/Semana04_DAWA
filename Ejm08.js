125825f56fe2e9470dab85b6d2ae6119f80d39a4
grmns <german.sandoval@tecsup.edu.pe>
2024-04-01 11:17:39 -0500

    Ejm08 IA

--- Ejm08.js
+++ Ejm08.js
@@ -1,12 +1,34 @@
+// const EventEmitter = require('events');
+// const myEmitter = new EventEmitter();
+
+// myEmitter.on('eventA', () =>{
+//     console.log('Listener 1 for eventA executed');
+// });
+
+// myEmitter.on('eventA', () =>{
+//     console.log('Listener 2 for eventA executed');
+// });
+
+// myEmitter.emit('eventA');
+
 const EventEmitter = require('events');
-const myEmitter = new EventEmitter();
 
-myEmitter.on('eventA', () =>{
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+// Definir funciones para manejar eventos
+function handleEventA() {
     console.log('Listener 1 for eventA executed');
-});
+}
 
-myEmitter.on('eventA', () =>{
+function handleEventASecond() {
     console.log('Listener 2 for eventA executed');
-});
+}
 
+// Registrar funciones de manejo de eventos
+myEmitter.on('eventA', handleEventA);
+myEmitter.on('eventA', handleEventASecond);
+
+// Emitir evento
 myEmitter.emit('eventA');
